71219348ad1ba7a3eb19aa08724d2f8f1cf2aaa4
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-05-28 18:11:45 -0400

    (feat): Added Structured Minimal Apis

--- Library.Api/ResultExtensions.cs
+++ Library.Api/ResultExtensions.cs
@@ -1,6 +1,29 @@
-﻿namespace Library.Api
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Library.Api
 {
-    public class ResultExtensions
+    public static class ResultExtensions
     {
+        public static IResult Html(this IResultExtensions extensions, string html)
+        {
+            return new HtmlResult(html);
+        }
+    }
+    public class HtmlResult : IResult
+    {
+        readonly string _html;
+        public HtmlResult(string html)
+        {
+            _html = html;
+        }
+        public Task ExecuteAsync(HttpContext httpContext)
+        {
+            httpContext.Response.ContentType = MediaTypeNames.Text.Html;
+            httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(_html);
+            return httpContext.Response.WriteAsync(_html, Encoding.UTF8);
+        }
     }
 }
